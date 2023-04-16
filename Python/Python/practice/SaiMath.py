baf7d98e4893cd495112597405b1a3cf7bb4e3a0
jojibab <jojibabu1043@gmail.com>
2023-04-16 12:19:13 +0530

    first commit

---
+++ Python/Python/practice/SaiMath.py
@@ -0,0 +1,13 @@
+# (SaiMath.py)
+a=100;
+b=200;
+def add(x,y):
+	print(x+y);
+def sub(x,y):
+	print(x-y);
+def prod(x,y):
+	print(x*y);
+def div(x,y):
+	print(x/y);	
+def mod(x,y):
+	print(x%y);
---
+++ Python/Python/practice/sample.py
@@ -0,0 +1,8 @@
+#single-value
+tup1=(10);
+print(tup1);
+print(type(tup1));
+print()
+tup1=(10,);
+print(tup1);
+print(type(tup1));
