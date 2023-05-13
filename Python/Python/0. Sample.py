e761965cd6515d074ff9260b302dee58a7600f56
jojibab <jojibabu1043@gmail.com>
2023-05-13 10:12:50 +0530

    added differencr between list,tuple,set,dic

---
+++ Python/Python/0. Sample.py
@@ -0,0 +1,36 @@
+'''List	                             Tuple	                                Set	                                        Dictionary
+List is a non-homogeneous           Tuple is also a non-homogeneous          Set data structure is also                 Dictionary is also a non-homogeneous 
+the elements in single row          row and multiple rows and columns       but stores in single row                    data structure which stores key value pairs
+data structure that stores          data structure that stores single       non-homogeneous data structure 
+and multiple rows and columns 
+
+List can be represented             Tuple can be represented                Set can be represented                      Dictionary  can be represented 
+by [ ]                              by ( )                                  by { }                                      by { }
+
+
+List allows                         Tuple allows                            Set will not allow                          Dictionary doesn’t 
+duplicate elements                  duplicate elements                      duplicate elements                          allow duplicate keys.
+
+
+List can use nested among all       Tuple can use nested among all          Set can use nested among all                Dictionary can use nested among all
+
+
+Example: [1, 2, 3, 4, 5]            Example: (1, 2, 3, 4, 5)                Example: {1, 2, 3, 4, 5}                    Example: {1: “a”, 2: “b”, 3: “c”, 4: “d”, 5: “e”}
+
+List can be created using           Tuple can be created using              Set can be created using                    Dictionary can be created using 
+list() function                     tuple() function.                       set() function                              dict() function
+
+
+List is mutable i.e we can          Tuple  is immutable i.e we can          Set is mutable i.e we can make any          Dictionary is mutable. But Keys 
+make any changes in list.           not make any changes in tuple           changes in set. But elements are 	        are not duplicated.
+                                    not duplicated.
+
+
+List is ordered	                    Tuple is ordered	                    Set is unordered	                        Dictionary is ordered (Python 3.7 and above)
+
+
+Creating an empty list              Creating an empty Tuple                 Creating a set a=set() b=set(a)             Creating an empty dictionary d={}
+l=[]                                t=()
+
+
+'''
